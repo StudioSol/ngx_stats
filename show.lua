31355d731b9b67476886e851b5b58d1e5227d2ae
Cícero Verneck Corrêa <cicerocomp@gmail.com>
2014-04-10 16:35:09 -0300

    Bug fix show not see result

--- show.lua
+++ show.lua
@@ -3,7 +3,7 @@ local keys = stats:get_keys()
 local response = {}
 
 for k,v in pairs(keys) do
-    response = common.format_response(k, v, response)
+    response = common.format_response(v, stats:get(v), response)
 end
 
 ngx.say(cjson.encode(response))
